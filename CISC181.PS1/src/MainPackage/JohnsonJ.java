0339cd1286bb7f4db8be01515187082f4105e505
Joshua Johnson <joshj@udel.edu>
2015-09-14 16:29:05 -0400

    Committed PS1

---
+++ CISC181.PS1/src/MainPackage/JohnsonJ.java
@@ -0,0 +1,9 @@
+package MainPackage;
+
+public class JohnsonJ {
+	public static void HelloWorld(){
+		
+		System.out.println("Hello World");
+	}
+
+}
---
+++ CISC181.PS1/src/MainPackage/Main.java
@@ -0,0 +1,12 @@
+package MainPackage;
+
+public class Main {
+	
+	/*My github account is: josh6694
+	*/
+	public static void main(String[] args) {
+			
+		JohnsonJ.HelloWorld();
+	}
+
+}
